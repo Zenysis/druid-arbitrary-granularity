4abc78cd3fc4bb1a10d8bb1681999ee35b5c4b4e
Stephen Ball <stephenball1@gmail.com>
2016-11-28 16:36:44 -0800

    Initial commit of the arbitrary granularity extension, including directions

--- README.md
+++ README.md
@@ -1,2 +1,116 @@
-# druid-arbitrary-granularity
-Druid extension that allows custom time intervals for a query's granularity
+## Druid Arbitrary Granularity Extension
+The arbitrary granularity extension is used for running [Druid](http://druid.io/)
+queries over custom time intervals. Currently, Druid requires a user to
+aggregate over fixed time windows (month, day, 36 hours, etc.). While easy to
+understand, this approach is fairly inflexible. This extension hopes to solve
+that problem.
+
+
+### Installation
+You can either use the prebuilt jar located in `dist/` or build from source
+using the directions below.
+
+Once you have the compiled `jar`, copy it to your druid installation and
+follow the
+[including extension](http://druid.io/docs/latest/operations/including-extensions.html)
+druid documentation. You should end up adding a line similar to this to your
+`common.runtime.properties` file:
+
+`druid.extensions.loadList=["druid-arbitrary-granularity"]`
+
+##### Building from source
+Clone the druid repo and this line to `pom.xml` in the "Community extensions"
+section:
+
+```xml
+<module>${druid-arbitrary-granularity-src-root}/arbitrary-granularity</module>
+```
+replacing `${druid-arbitrary-granularity-src-root}` with your path to this
+repo.
+
+Then, inside the druid repo, run:
+`mvn package -DskipTests=true -rf :druid-arbitrary-granularity`
+This will build the arbitrary granularity extension and place it in
+`${druid-arbitrary-granularity-src-root}/arbitrary-granularity/target`
+
+
+### Use
+The arbitrary granularity takes an array of time intervals which will define
+the buckets used during querying. It uses the same `intervals` format as the
+normal query interval.
+```json
+{
+  ...
+  "granularity": {
+    "type": "arbitrary",
+    "intervals": <Array of time intervals>
+  },
+  ...
+}
+```
+
+Only events that fall in one of the defined intervals will be added to the
+bucket. If the query interval is longer than any of the granularity intervals,
+events outside the granularity intervals (but within the query interval) will
+be dropped. If the query interval partially covers one of the granularity
+intervals, only the data that is both within the query interval AND within the
+granularity interval will be included.
+
+### Example
+
+##### Quarters that do not start at the beginning of the calendar year
+Often, a business's fiscal year does not align with a calendar year.
+Grouping by the builtin `quarter` would not work since it starts with
+Jan 1, and using the builtin `period` granularity won't work since
+each quarter is not the same length. The arbitrary granularity extension
+can fix this:
+
+Quarter start: October 1, 2015
+```json
+{
+  ...
+  "intervals": [
+    "2015-10-01/2016-01-01",
+    "2016-01-01/2016-04-01",
+    "2016-04-01/2016-07-01",
+    "2016-07-01/2016-10-01",
+  ],
+  "granularity": {
+    "type": "arbitrary",
+    "intervals": [
+      "2015-10-01/2016-01-01",
+      "2016-01-01/2016-04-01",
+      "2016-04-01/2016-07-01",
+      "2016-07-01/2016-10-01",
+    ]
+  },
+  ...
+}
+```
+
+##### Weekday vs Weekend
+Month: October 2016
+```json
+{
+  ...
+  "intervals": [
+    "2016-10-01/2016-11-05",
+  ],
+  "granularity": {
+    "type": "arbitrary",
+    "intervals": [
+      "2016-10-01/2016-10-03",
+      "2016-10-03/2016-10-08",
+      "2016-10-08/2016-10-10",
+      "2016-10-10/2016-10-15",
+      "2016-10-15/2016-10-17",
+      "2016-10-17/2016-10-22",
+      "2016-10-22/2016-10-24",
+      "2016-10-24/2016-10-29",
+      "2016-10-29/2016-10-31",
+      "2016-10-31/2016-11-05",
+    ]
+  },
+  ...
+}
+```
---
+++ arbitrary-granularity/src/main/java/io/druid/granularity/ArbitraryGranularity.java
@@ -0,0 +1,310 @@
+/*
+ * Licensed to Metamarkets Group Inc. (Metamarkets) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Metamarkets licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.druid.granularity;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterators;
+import com.google.common.collect.Lists;
+import com.google.common.collect.PeekingIterator;
+import com.google.common.collect.Sets;
+
+// TODO(stephen): Use these when our druid version is updated
+// import io.druid.common.utils.JodaUtils;
+// import io.druid.java.util.common.guava.Comparators;
+// import io.druid.java.util.common.StringUtils;
+
+import org.joda.time.DateTimeZone;
+import org.joda.time.Interval;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.TreeSet;
+
+// TODO(stephen): Remove these imports when our druid is updated
+import com.google.common.base.Charsets;
+import com.google.common.base.Throwables;
+import java.io.UnsupportedEncodingException;
+import java.util.Comparator;
+import org.joda.time.DateTimeComparator;
+
+
+public class ArbitraryGranularity extends BaseQueryGranularity
+{
+  private final TreeSet<Interval> intervals;
+  private final String timezone;
+
+  @JsonCreator
+  public ArbitraryGranularity(
+    @JsonProperty("intervals") List<Interval> inputIntervals,
+    @JsonProperty("timezone") String timezone
+  )
+  {
+    this.intervals = Sets.newTreeSet(Comparators.intervalsByStartThenEnd());
+    this.timezone = timezone;
+    final DateTimeZone timeZone = DateTimeZone.forID(this.timezone);
+
+    if (inputIntervals == null) {
+      inputIntervals = Lists.newArrayList();
+    }
+
+    Preconditions.checkArgument(inputIntervals.size() > 0,
+                                "at least one interval should be specified");
+
+    // Insert all intervals
+    for (final Interval inputInterval : inputIntervals) {
+      Interval adjustedInterval = inputInterval;
+      if (this.timezone != null) {
+        adjustedInterval = new Interval(inputInterval.getStartMillis(),
+                                        inputInterval.getEndMillis(), timeZone);
+      }
+      intervals.add(adjustedInterval);
+    }
+
+    // Ensure intervals are non-overlapping (but they may abut each other)
+    final PeekingIterator<Interval> intervalIterator =
+        Iterators.peekingIterator(intervals.iterator());
+    while (intervalIterator.hasNext()) {
+      final Interval currentInterval = intervalIterator.next();
+
+      if (intervalIterator.hasNext()) {
+        final Interval nextInterval = intervalIterator.peek();
+        if (currentInterval.overlaps(nextInterval)) {
+          throw new IllegalArgumentException(
+              String.format(
+                  "Overlapping granularity intervals: %s, %s",
+                  currentInterval,
+                  nextInterval
+              )
+          );
+        }
+      }
+    }
+  }
+
+  public ArbitraryGranularity(List<Interval> inputIntervals)
+  {
+    this(inputIntervals, null);
+  }
+
+  @JsonProperty("intervals")
+  public TreeSet<Interval> getIntervals()
+  {
+    return this.intervals;
+  }
+
+  @Override
+  public long next(long t)
+  {
+    // Test if the input cannot be bucketed
+    if (t > intervals.last().getEndMillis()) {
+      return JodaUtils.MAX_INSTANT;
+    }
+
+    // First interval with start time <= timestamp
+    final Interval interval = intervals.floor(
+      new Interval(t, JodaUtils.MAX_INSTANT, DateTimeZone.UTC)
+    );
+    return interval != null && interval.contains(t)
+            ? interval.getEndMillis()
+            : t;
+  }
+
+  @Override
+  public long truncate(final long t)
+  {
+    // Test if the input cannot be bucketed
+    if (t > intervals.last().getEndMillis()) {
+      return JodaUtils.MAX_INSTANT;
+    }
+
+    // Just return the input. The iterable override below will
+    // define the buckets that should be used.
+    return t;
+  }
+
+  @Override
+  public Iterable<Long> iterable(final long start, final long end)
+  {
+    // Return an empty iterable if the requested time interval does not
+    // overlap any of the arbitrary intervals specified
+    if (end < intervals.first().getStartMillis() ||
+        start > intervals.last().getEndMillis()) {
+      // Special case for GroupBy queries. GroupBy calls iterator().next()
+      // without first checking hasNext(). For these queries, we need to return
+      // at least one value so that a null pointer exception is not raised.
+      // Since we don't have a way of inspecting which query type is being
+      // performed, try to match the signature of GroupBy's call to iterable
+      // where the end timestamp is only one millisecond after the start
+      // timestamp.
+      // TODO(stephen): Remove this when the issue is resolved.
+      if (end - start == 1) {
+        return ImmutableList.of(JodaUtils.MIN_INSTANT);
+      }
+      return ImmutableList.of();
+    }
+
+    return new Iterable<Long>()
+    {
+      @Override
+      public Iterator<Long> iterator()
+      {
+        // Skip over the intervals that are known to be invalid
+        // because they end before the requested start timestamp
+        final PeekingIterator<Interval> intervalIterator =
+            Iterators.peekingIterator(intervals.iterator());
+        while (intervalIterator.hasNext() &&
+               intervalIterator.peek().getEndMillis() <= start) {
+          intervalIterator.next();
+        }
+
+        return new Iterator<Long>()
+        {
+          @Override
+          public boolean hasNext()
+          {
+            return intervalIterator.hasNext() &&
+                   intervalIterator.peek().getStartMillis() < end;
+          }
+
+          @Override
+          public Long next()
+          {
+            return intervalIterator.next().getStartMillis();
+          }
+
+          @Override
+          public void remove()
+          {
+            throw new UnsupportedOperationException();
+          }
+        };
+      }
+    };
+  }
+
+  @Override
+  public byte[] cacheKey()
+  {
+    return StringUtils.toUtf8(getPrettyIntervals());
+  }
+
+  @Override
+  public boolean equals(Object o)
+  {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    ArbitraryGranularity that = (ArbitraryGranularity) o;
+
+    if (!intervals.equals(that.intervals)) {
+      return false;
+    }
+    return !(timezone != null
+             ? !timezone.equals(that.timezone)
+             : that.timezone != null);
+  }
+
+  @Override
+  public int hashCode()
+  {
+    int result = intervals.hashCode();
+    result = 31 * result + (timezone != null ? timezone.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString()
+  {
+    return "ArbitraryGranularity{" +
+           "intervals=" + getPrettyIntervals() +
+           '}';
+  }
+
+  private String getPrettyIntervals()
+  {
+    StringBuilder bob = new StringBuilder('[');
+    boolean hit = false;
+    for (Interval interval : intervals) {
+      if (hit) {
+        bob.append(',');
+      }
+      bob.append(interval.toString());
+      hit = true;
+    }
+    bob.append(']');
+    return bob.toString();
+  }
+
+  // The classes below backfill in the classes that are available on 0.9.3+ but not
+  // on druid 0.9.1.1 that we run on prod.
+  // TODO(stephen): Remove when druid is updated
+  private static class Comparators
+  {
+    private static final Comparator<Interval> INTERVAL_BY_START_THEN_END = new Comparator<Interval>()
+    {
+      private final DateTimeComparator dateTimeComp = DateTimeComparator.getInstance();
+
+      @Override
+      public int compare(Interval lhs, Interval rhs)
+      {
+        int retVal = dateTimeComp.compare(lhs.getStart(), rhs.getStart());
+        if (retVal == 0) {
+          retVal = dateTimeComp.compare(lhs.getEnd(), rhs.getEnd());
+        }
+        return retVal;
+      }
+    };
+
+    public static Comparator<Interval> intervalsByStartThenEnd()
+    {
+      return INTERVAL_BY_START_THEN_END;
+    }
+  }
+
+  private static class JodaUtils
+  {
+    public static final long MAX_INSTANT = Long.MAX_VALUE / 2;
+    public static final long MIN_INSTANT = Long.MIN_VALUE / 2;
+  }
+
+  private static class StringUtils
+  {
+    public static final String UTF8_STRING = Charsets.UTF_8.toString();
+
+    public static byte[] toUtf8(final String string)
+    {
+      try {
+        return string.getBytes(UTF8_STRING);
+      }
+      catch (UnsupportedEncodingException e) {
+        // Should never happen
+        throw Throwables.propagate(e);
+      }
+    }
+  }
+}
