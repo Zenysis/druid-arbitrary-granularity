c6d56efb4a32d1b5d3abf1301c33cf3118cd7b73
Stephen Ball <stephenball1@gmail.com>
2018-12-10 13:07:04 -0800

    Prepare for druid 0.13.0 release

--- arbitrary-granularity/src/main/java/org/apache/druid/java/util/common/granularity/ArbitraryGranularity.java
+++ arbitrary-granularity/src/main/java/org/apache/druid/java/util/common/granularity/ArbitraryGranularity.java
@@ -1,18 +1,18 @@
 /*
- * Licensed to Metamarkets Group Inc. (Metamarkets) under one
- * or more contributor license agreements. See the NOTICE file
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership. Metamarkets licenses this file
+ * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
+ * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
@@ -20,7 +20,7 @@
 // Copyright 2017 Zenysis Inc. All Rights Reserved.
 // Author: stephen@zenysis.com (Stephen Ball)
 
-package io.druid.java.util.common.granularity;
+package org.apache.druid.java.util.common.granularity;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -32,10 +32,10 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.PeekingIterator;
 import com.google.common.collect.Sets;
 
-import io.druid.java.util.common.DateTimes;
-import io.druid.java.util.common.IAE;
-import io.druid.java.util.common.guava.Comparators;
-import io.druid.java.util.common.StringUtils;
+import org.apache.druid.java.util.common.DateTimes;
+import org.apache.druid.java.util.common.IAE;
+import org.apache.druid.java.util.common.guava.Comparators;
+import org.apache.druid.java.util.common.StringUtils;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
@@ -139,14 +139,6 @@ public class ArbitraryGranularity extends Granularity
     return interval != null && interval.contains(time)
             ? interval.getEnd()
             : time;
-  }
-
-  @Override
-  public DateTime decrement(DateTime time)
-  {
-    throw new UnsupportedOperationException(
-      "This method should not be invoked for this granularity type"
-    );
   }
 
   @Override
